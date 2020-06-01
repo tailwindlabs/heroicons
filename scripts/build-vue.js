7288bbedad686aa7f1248f6b94ff90d2447c7f83
Adam Wathan <adam.wathan@gmail.com>
2020-06-01 13:59:39 -0400

    Build Vue components as .vue files using templates for now at least

--- scripts/build-vue.js
+++ scripts/build-vue.js
@@ -16,53 +16,29 @@ rimraf('./vue/outline/*')
       fs.readdir('./solid').then((files) => {
         return Promise.all(
           files.map((file) => {
-            return fs
-              .readFile(`./solid/${file}`, 'utf8')
-              .then((content) => {
-                return svgToVue(content)
-              })
-              .then((component) => {
-                const fileName = `${camelcase(file.replace(/\.svg$/, ''), { pascalCase: true })}.js`
-                const content = dedent(component).replace('module.exports = ', 'export default ')
-                return fs.writeFile(`./vue/solid/${fileName}`, content).then(() => fileName)
-              })
-          })
-        ).then((fileNames) => {
-          const exportStatements = fileNames
-            .map((fileName) => {
-              const componentName = fileName.replace(/\.js$/, '')
-              return `export { default as ${componentName} } from './${fileName}'`
+            return fs.readFile(`./solid/${file}`, 'utf8').then((svg) => {
+              const fileName = `${camelcase(file.replace(/\.svg$/, ''), {
+                pascalCase: true,
+              })}.vue`
+              const content = `<template>${svg}</template>`
+              return fs.writeFile(`./vue/solid/${fileName}`, content).then(() => fileName)
             })
-            .join('\n')
-
-          return fs.writeFile('./vue/solid/index.js', exportStatements)
-        })
+          })
+        )
       }),
 
       fs.readdir('./outline').then((files) => {
         return Promise.all(
           files.map((file) => {
-            return fs
-              .readFile(`./outline/${file}`, 'utf8')
-              .then((content) => {
-                return svgToVue(content)
-              })
-              .then((component) => {
-                const fileName = `${camelcase(file.replace(/\.svg$/, ''), { pascalCase: true })}.js`
-                const content = dedent(component).replace('module.exports = ', 'export default ')
-                return fs.writeFile(`./vue/outline/${fileName}`, content).then(() => fileName)
-              })
-          })
-        ).then((fileNames) => {
-          const exportStatements = fileNames
-            .map((fileName) => {
-              const componentName = fileName.replace(/\.js$/, '')
-              return `export { default as ${componentName} } from './${fileName}'`
+            return fs.readFile(`./outline/${file}`, 'utf8').then((svg) => {
+              const fileName = `${camelcase(file.replace(/\.svg$/, ''), {
+                pascalCase: true,
+              })}.vue`
+              const content = `<template>${svg}</template>`
+              return fs.writeFile(`./vue/outline/${fileName}`, content).then(() => fileName)
             })
-            .join('\n')
-
-          return fs.writeFile('./vue/outline/index.js', exportStatements)
-        })
+          })
+        )
       }),
     ])
   })
