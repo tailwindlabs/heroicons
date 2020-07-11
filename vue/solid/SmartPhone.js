1d674a3371441af848bbd93f4d76440a71e90d86
Claret Nnamocha <devclareo@gmail.com>
2020-07-11 10:00:43 +0100

    add smart-phone icon

---
+++ vue/solid/SmartPhone.js
@@ -0,0 +1,11 @@
+import { createVNode as _createVNode, openBlock as _openBlock, createBlock as _createBlock } from "vue"
+
+export default function render(_ctx, _cache) {
+return (_openBlock(), _createBlock("svg", {
+  viewBox: "0 0 20 20",
+  fill: "currentColor"
+}, [
+  _createVNode("path", { d: "M14.688 19.063a.46.46 0 01-.47.468h-8.28a.46.46 0 01-.47-.468V17.03h9.22zm0-2.344h-9.22V3.28h9.22zm0-13.75h-9.22V.938a.46.46 0 01.47-.47h8.28a.46.46 0 01.47.47zm0 0" }),
+  _createVNode("path", { d: "M11.406 1.563H8.594c-.094 0-.156.062-.156.156 0 .093.062.156.156.156h2.812c.094 0 .156-.063.156-.156 0-.094-.062-.157-.156-.157zm0 0M10 19.219a.94.94 0 00.938-.938.94.94 0 00-.938-.937.94.94 0 00-.938.937.94.94 0 00.938.938zm0-1.563c.344 0 .625.282.625.625a.627.627 0 01-.625.625.627.627 0 01-.625-.625c0-.343.281-.625.625-.625zm0 0" })
+]))
+}
