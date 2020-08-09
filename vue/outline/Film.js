28085da5b75d89478d34ea0990cef00a1f2b61d0
sschoger <sm.schoger@gmail.com>
2020-08-09 14:43:11 -0400

    Fixed outline icons that rendered as filled
    Applied to most recent update

--- vue/outline/Film.js
+++ vue/outline/Film.js
@@ -8,6 +8,8 @@ return (_openBlock(), _createBlock("svg", {
   stroke: "currentColor"
 }, [
   _createVNode("path", {
+    "stroke-linecap": "round",
+    "stroke-linejoin": "round",
     "stroke-width": "2",
     d: "M7 4v16M17 4v16M3 8h4m10 0h4M3 12h18M3 16h4m10 0h4M4 20h16a1 1 0 001-1V5a1 1 0 00-1-1H4a1 1 0 00-1 1v14a1 1 0 001 1z"
   })
--- vue/outline/Table.js
+++ vue/outline/Table.js
@@ -8,6 +8,8 @@ return (_openBlock(), _createBlock("svg", {
   stroke: "currentColor"
 }, [
   _createVNode("path", {
+    "stroke-linecap": "round",
+    "stroke-linejoin": "round",
     "stroke-width": "2",
     d: "M3 10h18M3 14h18m-9-4v8m-7 0h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z"
   })
