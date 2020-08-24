674ebf24cc0be7e548db217ebcca835eb5af7645
sschoger <sm.schoger@gmail.com>
2020-08-24 13:18:41 -0400

    Fixed icons for small fire and shopping-bag

--- react/solid/ShoppingBag.jsx
+++ react/solid/ShoppingBag.jsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 
 function ShoppingBagIcon(props) {
-  return <svg viewBox="0 0 20 20" fill="currentColor" {...props}><path stroke="#374151" strokeLinejoin="round" strokeWidth={2} d="M5 8h10l1 9H4l1-9z" /><path stroke="#374151" strokeWidth={2} d="M7 6a3 3 0 013-3v0a3 3 0 013 3v3a3 3 0 01-3 3v0a3 3 0 01-3-3V6z" /><rect width={2} height={2} x={6} y={9} rx={1} /><rect width={2} height={2} x={12} y={9} rx={1} /></svg>;
+  return <svg viewBox="0 0 20 20" fill="currentColor" {...props}><path fillRule="evenodd" d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z" clipRule="evenodd" /></svg>;
 }
 
 export default ShoppingBagIcon;
--- vue/solid/ShoppingBag.js
+++ vue/solid/ShoppingBag.js
@@ -7,29 +7,9 @@ return (_openBlock(), _createBlock("svg", {
   fill: "currentColor"
 }, [
   _createVNode("path", {
-    stroke: "#374151",
-    "stroke-linejoin": "round",
-    "stroke-width": "2",
-    d: "M5 8h10l1 9H4l1-9z"
-  }),
-  _createVNode("path", {
-    stroke: "#374151",
-    "stroke-width": "2",
-    d: "M7 6a3 3 0 013-3v0a3 3 0 013 3v3a3 3 0 01-3 3v0a3 3 0 01-3-3V6z"
-  }),
-  _createVNode("rect", {
-    width: "2",
-    height: "2",
-    x: "6",
-    y: "9",
-    rx: "1"
-  }),
-  _createVNode("rect", {
-    width: "2",
-    height: "2",
-    x: "12",
-    y: "9",
-    rx: "1"
+    "fill-rule": "evenodd",
+    d: "M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z",
+    "clip-rule": "evenodd"
   })
 ]))
 }
