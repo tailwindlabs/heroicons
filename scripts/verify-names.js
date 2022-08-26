02e95bf680ff0da3e48b116e2260435fdd52b87c
Robin Malfait <malfait.robin@gmail.com>
2022-08-26 15:43:24 +0200

    add quick little lint script
    This will verify that all files exist in all variants

---
+++ scripts/verify-names.js
@@ -0,0 +1,40 @@
+const fs = require('fs').promises
+const path = require('path')
+
+const srcPaths = {
+  mini: path.resolve(__dirname, '../src/20/solid/'),
+  solid: path.resolve(__dirname, '../src/24/solid/'),
+  outline: path.resolve(__dirname, '../src/24/outline/'),
+}
+
+async function main() {
+  let files = await Promise.all(
+    Object.entries(srcPaths).map(async ([name, path]) => {
+      return { name, files: await fs.readdir(path) }
+    })
+  )
+
+  let diffs = []
+  for (let current of files) {
+    for (let other of files) {
+      if (current === other) continue
+
+      for (let file of current.files) {
+        if (!other.files.includes(file)) {
+          diffs.push({
+            package: current.name,
+            file: file,
+            'Missing in?': other.name,
+          })
+        }
+      }
+    }
+  }
+  if (diffs.length > 0) {
+    console.table(diffs)
+  } else {
+    console.log('All good!')
+  }
+}
+
+main()
